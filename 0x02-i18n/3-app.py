d1c8d38ca35ed661b220632f5f421618913865f5
12siragi <siragisaidi218@gmail.com>
2024-09-13 21:16:35 +0000

    pyt

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,42 +1,37 @@
 #!/usr/bin/env python3
+"""A Basic Flask app with internationalization support.
 """
-    Contains a basic flask app displaying 'Welcome to Holberton' on
-    a single route '/'
-"""
-
-
-from flask import Flask, render_template, request
 from flask_babel import Babel
-from os import getenv
+from flask import Flask, render_template, request
 
 
-app = Flask(__name__, static_url_path='')
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
-
-
-class Config(object):
-    """configuration for babel"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object('1-app.Config')
-
-
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """this route renders 0-index.html template"""
-    return render_template('3-index.html')
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """this method determine the best match with our supported languages"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
